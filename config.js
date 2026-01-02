2f25d11108219df7057c0aa27b5cc8138dbbffbc
Mick <mick.sung01@gmail.com>
2026-01-02 16:35:39 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  // 請將您的 Google Apps Script 部署網址 (Exec) 填入下方引號中
+  API_URL: "https://script.google.com/macros/s/AKfycbzR0jZoXnoTMxG5kcgPGj_IgZcfaa1ko5cbC0n0-A6QefxIYGwqMJH5R95rBOYkr7vNGQ/exec"
+};
